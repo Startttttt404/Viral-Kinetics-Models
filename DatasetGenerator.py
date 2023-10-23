2b07eae8e92b2300e6a0909891e3e8e5942f6e7b
huntstew <huntstew@iu.edu>
2023-10-23 11:43:11 -0400

    - fixed small glitch with dataset generation - change to a classifier model

--- DatasetGenerator.py
+++ DatasetGenerator.py
@@ -113,15 +113,13 @@ def generate_viral_kinetics_dataset(variable_elements, num_choices, delta_t, t_0
     datapoints = []
     for system in tqdm(systems, desc="Computing Solutions"):
         model = DDEViralKineticsModel(*system)
-        solution = model.solve(delta_t, t_0, t_n, Y_0)
+        solution = model.solve(0.001, t_0, t_n, Y_0)
 
-        counter1 = 0
-        counter2 = 1
-        while counter2 < len(solution):
-            x = solution[counter1]
-            y = solution[counter2]
-            counter1 += 1
-            counter2 += 1
+        counter = 0
+        while counter < len(solution) - int(delta_t / 0.001):
+            x = solution[counter]
+            y = solution[counter + int(delta_t / 0.001)]
+            counter += 1
             datapoints.append(np.append(x, y))
 
     df = pd.DataFrame(datapoints)
@@ -133,97 +131,8 @@ def generate_viral_kinetics_dataset(variable_elements, num_choices, delta_t, t_0
               index=False)
     print("Finished!")
 
-def generate_lstm_dataset(variable_elements, num_choices, delta_t, t_0, t_n):
-    variable_elements = [element.lower() for element in variable_elements]
-
-    beta = [6.2e-5]
-    k = [4.0]
-    p = [1.0]
-    c = [9.4]
-    delta = [2.4e-1]
-    delta_e = [1.9]
-    k_delta_e = [4.3e2]
-    xi = [2.6e4]
-    k_e = [8.1e5]
-    eta = [2.5e-7]
-    tau_e = [3.6]
-    d_e = [1.0]
-    zeta = [2.2e-1]
-    tau_m = [3.5]
-
-    actual_variable_elements = []
-    if 'beta' in variable_elements:
-        actual_variable_elements.append('beta')
-        beta.extend(generate_within_confidence(BETA_CONFIDENCE, num_choices))
-    if 'k' in variable_elements:
-        actual_variable_elements.append('k')
-        k.extend(generate_within_confidence(K_CONFIDENCE, num_choices))
-    if 'p' in variable_elements:
-        actual_variable_elements.append('p')
-        p.extend(generate_within_confidence(P_CONFIDENCE, num_choices))
-    if 'c' in variable_elements:
-        actual_variable_elements.append('c')
-        c.extend(generate_within_confidence(C_CONFIDENCE, num_choices))
-    if 'delta' in variable_elements:
-        actual_variable_elements.append('delta')
-        delta.extend(generate_within_confidence(DELTA_CONFIDENCE, num_choices))
-    if 'delta_e' in variable_elements:
-        actual_variable_elements.append('delta_e')
-        delta_e.extend(generate_within_confidence(DELTA_E_CONFIDENCE, num_choices))
-    if 'k_delta_e' in variable_elements:
-        actual_variable_elements.append('k_delta_e')
-        k_delta_e.extend(generate_within_confidence(K_DELTA_E_CONFIDENCE, num_choices))
-    if 'xi' in variable_elements:
-        actual_variable_elements.append('xi')
-        xi.extend(generate_within_confidence(XI_CONFIDENCE, num_choices))
-    if 'k_e' in variable_elements:
-        actual_variable_elements.append('k_e')
-        k_e.extend(generate_within_confidence(K_E_CONFIDENCE, num_choices))
-    if 'eta' in variable_elements:
-        actual_variable_elements.append('eta')
-        eta.extend(generate_within_confidence(ETA_CONFIDENCE, num_choices))
-    if 'tau_e' in variable_elements:
-        actual_variable_elements.append('tau_e')
-        tau_e.extend(generate_within_confidence(TAU_E_CONFIDENCE, num_choices))
-    if 'd_e' in variable_elements:
-        actual_variable_elements.append('d_e')
-        d_e.extend(generate_within_confidence(D_E_CONFIDENCE, num_choices))
-    if 'zeta' in variable_elements:
-        actual_variable_elements.append('zeta')
-        zeta.extend(generate_within_confidence(ZETA_CONFIDENCE, num_choices))
-    if 'tau_m' in variable_elements:
-        actual_variable_elements.append('tau_m')
-        tau_m.extend(generate_within_confidence(TAU_M_CONFIDENCE, num_choices))
-
-    systems = []
-    for system_permutation in product(beta, k, p, c, delta, delta_e, k_delta_e, xi, k_e, eta, tau_e, d_e, zeta, tau_m,
-                                      desc="Computing Permutations"):
-        systems.append(system_permutation)
-
-    datapoints = []
-    for system in tqdm(systems, desc="Computing Solutions"):
-        model = DDEViralKineticsModel(*system)
-        solution = model.solve(delta_t, t_0, t_n, Y_0)
-
-        counter1 = 0
-        counter2 = 1
-        while counter2 < len(solution):
-            x = solution[counter1]
-            y = solution[counter2]
-            counter1 += 1
-            counter2 += 1
-            datapoints.append(np.append(x, y))
-
-    df = pd.DataFrame(datapoints)
-
-    print("Saving CSV.")
-    df.to_csv(generate_file_name(actual_variable_elements, num_choices, delta_t, t_0, t_n),
-              header=["xTarget", "xPre-Infected", "xInfected", "xVirus", "xCDE8e", "xCD8m",
-                      "yTarget", "yPre-Infected", "yInfected", "yVirus", "yCDE8e", "yCD8m"],
-              index=False)
-    print("Finished!")
 
 if __name__ == '__main__':
-    generate_viral_kinetics_dataset([], 0, 0.01, 0, 6)
+    generate_viral_kinetics_dataset(['delta', 'd_e', 'eta'], 1, 1, 0, 12)
     #generate_viral_kinetics_dataset(['delta', 'd_e', 'eta'], 5, 0.01, 0, 12)
 
